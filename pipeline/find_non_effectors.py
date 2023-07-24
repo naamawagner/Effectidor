0969a403c3627003a0921773d37f00f78949ae37
naamawagner <naamawagner@mail.tau.ac.il>
2023-07-24 15:22:21 +0200

    bug fix
    Removal of "effectors" that have better hits to E. coli K12 should be done only in the case the effectors were not chosen based on homology search.

--- pipeline/find_non_effectors.py
+++ pipeline/find_non_effectors.py
@@ -51,26 +51,28 @@ def parse_blast_out(blast_out,e_val=0.01,min_coverage=0):
                         blast_out_dic[prot_id][0].append(row[1])
     return blast_out_dic
 
-effectors = {}
-with open('blast_outputs/effectors_hits.csv') as in_f:
-    reader = csv.reader(in_f)
-    for row in reader:
-        effectors[row[0]]=float(row[1])
+if os.path.exists('blast_outputs/effectors_hits.csv'):
+    effectors = {}
+    with open('blast_outputs/effectors_hits.csv') as in_f:
+        reader = csv.reader(in_f)
+        for row in reader:
+            effectors[row[0]]=float(row[1])
 
 prots_dict=SeqIO.to_dict(SeqIO.parse(all_prots,'fasta'))
 
 protein_blast_all_vs_all(all_prots,k12_dataset,k12_out_file)
 k12_dict = parse_blast_out(k12_out_file,e_val=10**(-6))
-effectors_to_keep = []
-for effector in effectors:
-    if effector not in k12_dict:
-        effectors_to_keep.append(effector)
-    elif effectors[effector] > k12_dict[effector][1]:
-        effectors_to_keep.append(effector)
-if len(effectors_to_keep) < len(effectors.keys()):
-    old_effectors = SeqIO.parse(effectors_prots,'fasta')
-    new_effectors = [effector for effector in old_effectors if effector.id in effectors_to_keep]
-    SeqIO.write(new_effectors,effectors_prots,'fasta')
+if os.path.exists('blast_outputs/effectors_hits.csv'):
+    effectors_to_keep = []
+    for effector in effectors:
+        if effector not in k12_dict:
+            effectors_to_keep.append(effector)
+        elif effectors[effector] > k12_dict[effector][1]:
+            effectors_to_keep.append(effector)
+    if len(effectors_to_keep) < len(effectors.keys()):
+        old_effectors = SeqIO.parse(effectors_prots,'fasta')
+        new_effectors = [effector for effector in old_effectors if effector.id in effectors_to_keep]
+        SeqIO.write(new_effectors,effectors_prots,'fasta')
         
 k12_out_list=list(k12_dict.keys())
 
