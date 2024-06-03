78fd5b26c3558c15b8d9029de4054f73add3a9bb
naamawagner <naamawagner@mail.tau.ac.il>
2024-06-03 14:02:49 +0300

    bug fix - limiting mmseqs2 to a single thread

--- pipeline/protein_mmseq.py
+++ pipeline/protein_mmseq.py
@@ -1,6 +1,6 @@
 
 def protein_mmseqs_all_vs_all(query, dataset, out, mmseq_tmp):
     import subprocess
-    command = f'mmseqs easy-search {query} {dataset} {out} {mmseq_tmp}'
+    command = f'mmseqs easy-search {query} {dataset} {out} {mmseq_tmp} --threads 1'
     subprocess.check_output(command,shell=True)
 
